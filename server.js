4d7c2bee95e94cd5c967a0bf8973151e1ff25b23
mustafausama <mustafausama@outlook.com>
2023-02-28 13:50:03 +0200

    fix: PORT instead of port

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 
 const app = express();
 
-const port = process.env.PORT || 4000;
+const PORT = process.env.PORT || 4000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -11,6 +11,6 @@ app.get("/", (req, res) => {
   res.send("Hello!!");
 });
 
-app.listen(port, () => {
-  console.log(`App is up and listening on port ${port}`);
+app.listen(PORT, () => {
+  console.log(`App is up and listening on port ${PORT}`);
 });
