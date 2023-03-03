4a2b9fa1ed99d430a80d1ca85fd20a25fd5ca404
mustafausama <mustafausama@outlook.com>
2023-03-03 20:34:05 +0200

    ui: Added description

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,21 @@
 import "./App.css";
 import React from "react";
 import MetaConnect from "./components/MetaConnect";
+import Container from "react-bootstrap/esm/Container";
 
 function App() {
   return (
     <div className="App">
+      <Container>
+        <h2 className="mt-4">Openstack ContribXP App</h2>
+        <p>
+          Contribute to our DAO’s open source repositories to be qualified to
+          earn experience points in our quest. Other members of the community
+          will review your Pull Request. Should it be accepted, your
+          contributions will be rewarded fairly based on the experience and
+          reputation power of the reviewers.
+        </p>
+      </Container>
       <MetaConnect />
       {/* <ToastContainer /> */}
     </div>
